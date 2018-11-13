b66982e83cdb72bc3975e1c8addafaf81ced8b18
Cris <22254235+crislin2046@users.noreply.github.com>
2018-11-13 05:28:55 +0000

    AuthIn looks ok.

--- src/views/AuthIn.js
+++ src/views/AuthIn.js
@@ -3,7 +3,7 @@ import {R,X} from '../../node_modules/brutalist-web/r.js';
 export default function AuthIn(state) {
   return R`
     <section class="authin">
-      <a href=https://github.com/login/oauth/authorize?client_id=d10c47b12243b3cdfd86>Login</a>
+      <a href=https://github.com/login/oauth/authorize?client_id=d10c47b12243b3cdfd86&scope=gist>Login</a>
     </section>
   `;
 }
